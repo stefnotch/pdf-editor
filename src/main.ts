8e49ccb03777b158b9f99ad32a0e81665d205a0e
sueszli <jabaryyahya@gmail.com>
2022-08-04 18:26:23 +0200

    add UI library via npm (still needs config for tree shaking)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+// external imports
 import { createApp } from "vue";
+import naive from "naive-ui";
+
+// internal imports
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
-import "./assets/main.css";
+// styling
+import "./styling/base.css";
 
 const app = createApp(App);
-
 app.use(createPinia());
-
+app.use(naive);
 app.mount("#app");
