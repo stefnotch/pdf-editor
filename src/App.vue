9e8cc855b27cd4a9568fe76cc9cdb6c87094bb9f
stefnotch <brandmairstefan@gmail.com>
2022-08-05 13:21:51 +0200

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <h1>Hello world!</h1>
-  {{ x }}
-  <n-button type="primary" @click="onClick"> Click me to hear something nice </n-button>
   <PdfEditor></PdfEditor>
 </template>
 
 <script lang="ts" setup>
 import PdfEditor from "./components/PdfEditor.vue";
-const x = ref("test");
-function onClick() {
-  alert("You smell funny!");
-}
 </script>
