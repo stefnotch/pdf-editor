2861cce72c08ad2c55cb87f50b4993a967904ea3
stefnotch <brandmairstefan@gmail.com>
2022-08-06 11:32:02 +0200

    Add pdfjs

--- src/components/PdfEditor.vue
+++ src/components/PdfEditor.vue
@@ -5,4 +5,13 @@
 import { useDocumentSessionStore } from "@/stores/document-sessions.js";
 
 const documentSessionStore = useDocumentSessionStore();
+
+// TODO: when rendering, make sure that it looks good on
+// - high DPI displays
+// - at any zoom level (and doesn't mess up when reloading while zoomed in/out)
+// - when Windows is set to a custom scale level
+// See:
+// https://github.com/mozilla/pdf.js/issues/10679#issuecomment-867541293
+// https://github.com/mozilla/pdf.js/issues/10509
+// https://github.com/mozilla/pdf.js/issues/7630
 </script>
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,20 @@ import App from "./App.vue";
 // styling
 import "./styling/base.css";
 
+// pdf.js
+import * as pdfjs from "pdfjs-dist";
+
+// TODO: Get rid of this disgusting hack once Firefox gets worker modules support.
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1247687
+import PdfjsWorker from "pdfjs-dist/build/pdf.worker.js?worker&url";
+pdfjs.GlobalWorkerOptions.workerPort = new Worker(PdfjsWorker.replace(/type=module/, ""), {
+  type: "classic",
+});
+/* The good approach:
+import PdfjsWorker from "pdfjs-dist/build/pdf.worker.js?worker";
+pdfjs.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+ */
+
 const app = createApp(App);
 app.use(createPinia());
 app.mount("#app");
