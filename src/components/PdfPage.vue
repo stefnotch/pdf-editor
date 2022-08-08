59730bab5a739bba8e862cd1991f94c7ad436594
stefnotch <brandmairstefan@gmail.com>
2022-08-08 23:39:19 +0200

    Primitive page rendering

--- src/components/PdfPage.vue
+++ src/components/PdfPage.vue
@@ -1,7 +1,10 @@
 <template>
   <!-- TODO: Render the page -->
   <!-- TODO: Only render the page when this element is visible -->
-  <div>page {{ page.pageIndex + 1 }}</div>
+  <div>
+    page {{ page.pageIndex + 1 }}
+    <canvas ref="pageCanvas"></canvas>
+  </div>
 </template>
 <script setup lang="ts">
 import { useDocumentSessionStore, type Page } from "@/stores/document-sessions";
@@ -10,7 +13,25 @@ const props = defineProps<{
   page: Page;
 }>();
 
+const pageCanvas = ref<HTMLCanvasElement | null>(null);
+
 const documentSessionStore = useDocumentSessionStore();
 
 const pageToRender = computed(() => documentSessionStore.getRenderedPage(props.page));
+
+watch([pageCanvas, pageToRender], ([canvas, page]) => {
+  if (canvas && page) {
+    const context = canvas.getContext("2d");
+    if (context) {
+      const baseViewport = page.getViewport({ scale: 1 });
+      const scaledViewport = baseViewport.clone({ scale: 1 }); // TODO: Scale the viewport (shrink to fit)
+      canvas.height = scaledViewport.width;
+      canvas.width = scaledViewport.height;
+      page.render({
+        canvasContext: context,
+        viewport: scaledViewport,
+      });
+    }
+  }
+});
 </script>
