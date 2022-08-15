11458a2768d7dabbd59db3edbb8649516908b757
stefnotch <brandmairstefan@gmail.com>
2022-08-15 17:39:06 +0200

    Minor tweaks

--- src/components/PdfPage.vue
+++ src/components/PdfPage.vue
@@ -24,14 +24,6 @@ const pageToRender = computed(() =>
   documentSessionStore.getRenderedPage(props.page)
 );
 
-// TODO: when rendering, make sure that it looks good on
-// - high DPI displays, when Windows is set to a custom scale level
-// - at any zoom level (and doesn't mess up when reloading while zoomed in/out)
-// See:
-// https://github.com/mozilla/pdf.js/issues/10679#issuecomment-867541293
-// https://github.com/mozilla/pdf.js/issues/10509
-// https://github.com/mozilla/pdf.js/issues/7630
-
 let cancelRenderTask: () => Promise<void> = () => Promise.resolve();
 watch(
   [pageCanvas, pageToRender, () => props.bounds, pixelRatio],
