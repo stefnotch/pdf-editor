9aa04e3638bc6303847e718884dd13ed05aa67ec
stefnotch <brandmairstefan@gmail.com>
2022-08-16 12:39:57 +0200

    Make GitHub link open in new tab

--- src/components/TheBottomBar.vue
+++ src/components/TheBottomBar.vue
@@ -6,7 +6,7 @@ import { version, homepage } from "../../package.json";
   <div class="border-t-1 flex flex-row p-b-1 p-x-2">
     <span>PDF Editor v{{ version }}</span>
     <span class="m-l-auto">
-      <a :href="homepage">GitHub</a>
+      <a :href="homepage" target="_blank">GitHub</a>
     </span>
   </div>
 </template>
